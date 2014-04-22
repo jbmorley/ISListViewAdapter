db6ae66275f59fa9dad14d2dbc2b47d33a0bffe9
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-04-22 11:16:39 +0200

    Cleaning up the logging interface.

---
+++ ISListViewAdapterLogger.h
@@ -0,0 +1,13 @@
+//
+//  ISListViewAdapterLogger.h
+//  Pods
+//
+//  Created by Jason Barrie Morley on 22/04/2014.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ISListViewAdapterLogger <NSObject>
+
+@end
