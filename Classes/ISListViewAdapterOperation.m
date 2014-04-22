b793c439d5064631a8b88ba02b4f8dd70e1b1fff
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-04-22 10:35:43 +0200

    Further copyright fixes.

--- Classes/ISListViewAdapterOperation.m
+++ Classes/ISListViewAdapterOperation.m
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2013 InSeven Limited.
+// Copyright (c) 2013-2014 InSeven Limited.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -19,6 +19,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //
+
 
 #import "ISListViewAdapterOperation.h"
 
